c8d571a44de2719d54d818d05df5822ac926c8b9
OmarShalaby24 <ramo_24@outlook.com>
2023-08-05 19:04:14 +0300

    update templates + customize font size

--- CertificateController.py
+++ CertificateController.py
@@ -46,21 +46,32 @@ class CertificateController:
         if self.data["courseCode"] in nonExpireCourses:
             self.data["Expire_date"] = "--/--/----"
             
-        def formatName(str, length):
-            if len(str) > length:
+        def formatName(str, len1, len2):
+            if len(str) >= len2:
                 size = 10*2
+            elif len(str) >= len1:
+                size = 12*2
             else:
                 size = 14*2
-            return RichText(str, size=size)
-        
+            return RichText(str, size=size, bold=True)
+        # removes zeros from the date
+        def dateFormater(datestr):
+            if datestr[0] == "0":
+                datestr = datestr[1:]
+            datestr = datestr.replace("/0", "/")
+            datestr = datestr.replace("/", "/ ")
+            return datestr
         #TODO: uncomment the following lines when adding {{r <var> }} in the template
         # ask for the max length of the name in English and Arabic
+        # self.data["Date_of_Birth"] = dateFormater(self.data["Date_of_Birth"])
+        # self.data["Issue_date"] = dateFormater(self.data["Issue_date"])
+        # self.data["Expire_date"] = dateFormater(self.data["Expire_date"])
+        # self.data["From"] = dateFormater(self.data["From"])
+        # self.data["To"] = dateFormater(self.data["To"])
         context = {
             "CertNo": self.data["courseCode"] + str(self.data["CertNo"]),
-            # "Name_En": formatName(self.data["Name_En"], 30),
-            # "Name_Ar": formatName(self.data["Name_Ar"], 35),
-            "Name_En": self.data["Name_En"],
-            "Name_Ar": self.data["Name_Ar"],
+            "Name_En": formatName(self.data["Name_En"], 40, 45),
+            "Name_Ar": formatName(self.data["Name_Ar"], 35, 40),
             "Place_of_Birth_En": self.data["Place_of_Birth_En"],
             "Place_of_Birth_Ar": self.data["Place_of_Birth_Ar"],
             "Date_of_Birth_En": self.data["Date_of_Birth"],
@@ -97,6 +108,8 @@ class CertificateController:
         if not os.path.exists(os.getcwd() + "/Certificates"):
             os.mkdir(os.getcwd() + "/Certificates")
         
+    
+
     # def selectTemplate(self, code):
     #     if code in self.Loader.getCertificateType()["Brackets"]:
     #         return "Brackets.docx"
@@ -118,25 +131,25 @@ class CertificateController:
     #         return "its ECDIS Management"
 
 
-# data = {
-#     "courseCode": "XNFF",
-#     "CertNo": 123,
-#     "Name_En": "Omar Ahmed Mahmoud Ahmed Mahmoud Shalaby".upper(),
-#     "Name_Ar": "عمر أحمد محمود أحمد محمود شلبي",
-#     "Place_of_Birth_En": "Alex".upper(),
-#     "Place_of_Birth_Ar": "الإسكندرية",
-#     "Date_of_Birth": "01/01/1999",
-#     "FromWhere_En": "Personal",
-#     "FromWhere_Ar": "شخصي",
-#     "Course_En": "Fire Fighting",
-#     "Course_Ar": "الإطفاء",
-#     "From": "01/01/2020",
-#     "To": "02/01/2020",
-#     "Rules": "A - VII / 1-1",
-#     "Expire_date": "01/01/2025",
-#     "Issue_date": "04/01/2020",
-#     "RegNo": "H00123456",
-# }
+data = {
+    # "courseCode": "XNFF",
+    "CertNo": 123,
+    "Name_En": "Omar Ahmed Mahmoud Ahmed Mahmoud Shalaby".upper(),
+    "Name_Ar": "عمر أحمد محمود أحمد محمود شلبي",
+    "Place_of_Birth_En": "Alex".upper(),
+    "Place_of_Birth_Ar": "الإسكندرية",
+    "Date_of_Birth": "24/9/1999",
+    "FromWhere_En": "Personal",
+    "FromWhere_Ar": "شخصي",
+    # "Course_En": "Fire Fighting",
+    # "Course_Ar": "الإطفاء",
+    "From": "01/01/2020",
+    "To": "02/01/2020",
+    # "Rules": "A - VII / 1-1",
+    "Expire_date": "01/01/2025",
+    "Issue_date": "04/01/2020",
+    "RegNo": "H00123456",
+}
 
 
 # loader = ld.LoadData()
@@ -158,3 +171,26 @@ class CertificateController:
 # generator = CertificateController(
 #     data, "D:\Projects\Python\MTUC Project\\fire fighting"
 # ).generateCertificate()
+
+if __name__ == "__main__":
+    loader = ld.LoadData()
+    courseslist = loader.getCoursesList()
+    # print(courseslist)
+    Courses_En = [x.split(" - ")[0] for x in courseslist]
+    Courses_Ar = [x.split(" - ")[1].split(" (")[0] for x in courseslist]
+    Courses_Code = [x.split(" - ")[1].split(" (")[-1].split(")")[0] for x in courseslist]
+    path = "results"
+    data["courseCode"] = Courses_Code[0]
+    data["Course_En"] = Courses_En[0]
+    data["Course_Ar"] = Courses_Ar[0]
+    data["Rules"] = loader.getRulesCode(Courses_En[0])
+    # generator = CertificateController(data, path)
+    # generator.generateCertificate()
+    for i in range(len(Courses_En)):
+        data["courseCode"] = Courses_Code[i]
+        data["Course_En"] = Courses_En[i]
+        data["Course_Ar"] = Courses_Ar[i]
+        data["Rules"] = loader.getRulesCode(Courses_En[i])
+        generator = CertificateController(data, path)
+        generator.generateCertificate()
+        print(f"{i} Done")
